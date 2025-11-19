3961b5fbcfa955fdec42ff2240ff6dc50298fc3d
Anastasiia Chaplyhina <chaplyhina.anastasiia@gmail.com>
2025-11-19 17:52:34 +0100

    Pagination+searchBox

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,56 @@
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { useDebounce } from 'use-debounce'; 
 import { fetchNotes } from '../../services/noteService';
 import NoteList from '../NoteList/NoteList';
+import SearchBox from '../SearchBox/SearchBox';
+import Pagination from '../Pagination/Pagination';
 import css from './App.module.css';
 
 const App = () => {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  
+  const [debouncedSearch] = useDebounce(search, 500);
+
+  const handleSearchChange = (newValue: string) => {
+    setSearch(newValue);
+    setPage(1); 
+  };
 
   const { data, isLoading, isError } = useQuery({
-    queryKey: ['notes', 1], 
-    queryFn: () => fetchNotes({ page: 1, perPage: 9 }),
+    queryKey: ['notes', page, debouncedSearch], 
+    queryFn: () => fetchNotes({ 
+      page, 
+      perPage: 9, 
+      search: debouncedSearch 
+    }),
   });
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <h2>My Notes</h2>
+
+        <SearchBox value={search} onChange={handleSearchChange} />
+        
+        <button className={css.createBtn} disabled>Create note +</button>
       </header>
 
       {isLoading && <p style={{ textAlign: 'center' }}>Loading notes...</p>}
       
       {isError && <p style={{ textAlign: 'center', color: 'red' }}>Error loading notes!</p>}
 
-      {data && <NoteList notes={data.notes} />}
+      {data && (
+        <>
+          <NoteList notes={data.notes} />
+          
+          <Pagination 
+            totalPages={data.totalPages} 
+            currentPage={page} 
+            onPageChange={setPage} 
+          />
+        </>
+      )}
     </div>
   );
 };
