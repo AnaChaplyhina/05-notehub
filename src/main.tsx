3fc078ad85283f3e036c44506027700f46bdbce3
Anastasiia Chaplyhina <chaplyhina.anastasiia@gmail.com>
2025-11-19 17:21:04 +0100

    main push_node,types, vite

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'modern-normalize'; 
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <h1>Привіт! Проєкт працює!</h1>
+  </React.StrictMode>
+);
